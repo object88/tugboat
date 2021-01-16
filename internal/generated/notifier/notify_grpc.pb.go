ecec5f81240f7d2056eafebbfabeb36bc08c0984
Paul Brousseau <object88@gmail.com>
2021-01-16 15:20:47 -0800

    chore: update k8s, uuid, grpc, k8s controller runtime

--- internal/generated/notifier/notify_grpc.pb.go
+++ internal/generated/notifier/notify_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // ListenerClient is the client API for Listener service.
@@ -62,7 +63,7 @@ type UnsafeListenerServer interface {
 }
 
 func RegisterListenerServer(s grpc.ServiceRegistrar, srv ListenerServer) {
-	s.RegisterService(&_Listener_serviceDesc, srv)
+	s.RegisterService(&Listener_ServiceDesc, srv)
 }
 
 func _Listener_OpenDeployment_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -83,7 +84,10 @@ func _Listener_OpenDeployment_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Listener_serviceDesc = grpc.ServiceDesc{
+// Listener_ServiceDesc is the grpc.ServiceDesc for Listener service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Listener_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "notifier.Listener",
 	HandlerType: (*ListenerServer)(nil),
 	Methods: []grpc.MethodDesc{
